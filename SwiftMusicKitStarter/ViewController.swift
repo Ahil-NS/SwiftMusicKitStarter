78b0b9d3affafbaaabb1c6b9e45d97f9385b8e1f
a <eahilendran@gmail.com>
2019-01-26 23:21:36 +0530

    first initial commit

---
+++ SwiftMusicKitStarter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftMusicKitStarter
+//
+//  Created by MacBook on 1/26/19.
+//  Copyright © 2019 Ahil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
