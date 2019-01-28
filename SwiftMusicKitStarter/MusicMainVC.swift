9e2b43494137c3dd1560d05f40731a2c14c73723
a <eahilendran@gmail.com>
2019-01-28 14:36:52 +0530

    Basic UI Designed

---
+++ SwiftMusicKitStarter/MusicMainVC.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  SwiftMusicKitStarter
+//
+//  Created by MacBook on 1/26/19.
+//  Copyright © 2019 Ahil. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class MusicMainVC: UIViewController {
+
+    @IBOutlet weak var songImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func chooseButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func randomButtonTapped(_ sender: Any) {
+        
+    }
+    @IBAction func playButtonTapped(_ sender: Any) {
+        
+    }
+}
+
